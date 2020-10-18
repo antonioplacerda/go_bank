e190dee86d25d7bbea91683a745f880dafbd2aed
Antonio P Lacerda <antonioplacerda@gmail.com>
2020-10-18 22:20:11 +0100

    [util] bugfix RandomStr len

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -22,7 +22,7 @@ func RandomStr(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
 
-	for i := 0; i < k; i++ {
+	for i := 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
